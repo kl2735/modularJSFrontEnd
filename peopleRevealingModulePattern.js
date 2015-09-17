0d7839bf38ba18c50e3bcb0d1ae7b3b4441f6c71
Ke Liao <kliao@appnexus.com>
2015-09-17 17:53:05 -0400

    initialize

---
+++ peopleRevealingModulePattern.js
@@ -0,0 +1,55 @@
+var people = ( function() {
+
+		var people = ["will", "ke", "leo"];
+		var getDom = function () {
+			this.$el = $('#peopleModule');
+			this.$button = this.$el.find('button');
+			this.$input = this.$el.find('input');
+			this.$ul = this.$el.find('ul');
+			this.template = this.$el.find('#people-template').html();
+		};
+
+		var init = function (){
+			getDom();
+			bindEvents();
+			_render();
+
+		};
+
+		var bindEvents = function () {
+			this.$button.on('click', addPerson.bind(this));
+			this.$ul.delegate('i.del', 'click', deletePerson.bind(this));
+		};
+
+		var addPerson = function(value) {
+			people.push( (typeof value === "string")? value : this.$input.val());
+			_render();
+			this.$input.val('');
+		};
+
+		var deletePerson = function (e) {
+			var $remove = $(e.target).closest('li');
+			var i = this.$ul.find('li').index($remove);
+			people.splice(i, 1);
+			_render();
+
+		};
+
+
+		var _render = function (){
+			var data = {
+				people: people
+			};
+
+			this.$ul.html(Mustache.render(this.template, data));
+			events.emit('peopleChanged', people.length);
+		};
+
+		init();
+
+		return {
+			addPerson: addPerson,
+			deletePerson: deletePerson
+		};
+
+})();
---
+++ stat.js
@@ -0,0 +1,28 @@
+
+var stat = (function () {
+	var peopleCount = 0;
+
+	var $stat = $('#statModule');
+	var template = $('#stat-template').html();
+
+	events.on('peopleChanged', setCount);
+
+	function render(){
+		console.log(peopleCount);
+		var data = {
+			stat: peopleCount
+		};
+		$stat.html(Mustache.render(template, data));
+	}
+
+	function setCount(num){
+		peopleCount = num;
+		render();
+	}
+
+	render();
+
+	return {
+		setCount: setCount
+	};
+})();
